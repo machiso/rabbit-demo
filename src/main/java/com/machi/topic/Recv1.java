ea588009b233512e5e64bb1ff3ccf0f48f1c0474
machi <machi@jcfc.cn>
2018-07-04 11:09:31 +0800

    topic模式

---
+++ src/main/java/com/machi/topic/Recv1.java
@@ -0,0 +1,47 @@
+package com.machi.topic;
+
+import com.machi.util.ConnectionUtils;
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Recv1 {
+
+    private static final String QUEUE_NAME="topic_1";
+    private static final String EXCHANGE_NAME="channel_topic";
+
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        final Connection connection = ConnectionUtils.getConnection();
+        final Channel channel = connection.createChannel();
+
+        //声明队列
+        channel.queueDeclare(QUEUE_NAME,false,false,false,null);
+        //绑定队列到交换机
+        channel.queueBind(QUEUE_NAME,EXCHANGE_NAME,"goods.add");
+
+        channel.basicQos(1);
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope,
+                                       AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String msg = new String(body,"utf-8");
+                System.out.println("[Recv1]:"+msg);
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    System.out.println("[Recv1] done");
+                    channel.basicAck(envelope.getDeliveryTag(),false);
+                }
+            }
+        };
+
+        //自动确认关闭，，改成手动确认
+        boolean autoAck = false;
+        channel.basicConsume(QUEUE_NAME,autoAck,consumer);
+}
+}
---
+++ src/main/java/com/machi/topic/Recv2.java
@@ -0,0 +1,49 @@
+package com.machi.topic;
+
+import com.machi.util.ConnectionUtils;
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Recv2 {
+
+    private static final String QUEUE_NAME="topic_2";
+    private static final String EXCHANGE_NAME="channel_topic";
+
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        final Connection connection = ConnectionUtils.getConnection();
+        final Channel channel = connection.createChannel();
+
+        //声明队列
+        channel.queueDeclare(QUEUE_NAME,false,false,false,null);
+        //绑定队列到交换机
+        channel.queueBind(QUEUE_NAME,EXCHANGE_NAME,"goods.#");
+//        channel.queueBind(QUEUE_NAME,EXCHANGE_NAME,"error");
+//        channel.queueBind(QUEUE_NAME,EXCHANGE_NAME,"warning");
+
+        channel.basicQos(1);
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope,
+                                       AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String msg = new String(body,"utf-8");
+                System.out.println("[Recv2]:"+msg);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    System.out.println("[Recv2] done");
+                    channel.basicAck(envelope.getDeliveryTag(),false);
+                }
+            }
+        };
+
+        //自动确认关闭，，改成手动确认
+        boolean autoAck = false;
+        channel.basicConsume(QUEUE_NAME,autoAck,consumer);
+}
+}
